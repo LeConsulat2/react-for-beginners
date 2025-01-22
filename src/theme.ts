f9ae171f312542875d569e183fcda65db43939e5
LeConsulat2 <wooine2@gmail.com>
2025-01-22 19:27:03 +1300

    Styling done

--- src/theme.ts
+++ src/theme.ts
@@ -3,7 +3,7 @@ import { DefaultTheme } from 'styled-components';
 const Theme: DefaultTheme = {
   bgColor: '#2f3640',
   textColor: '#ecf0f1',
-  accentColor: '#ecf0f1',
+  accentColor: 'cyan',
 };
 
 export { Theme };
